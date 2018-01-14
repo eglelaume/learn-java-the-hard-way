97bfc59775afe938088a0baf639d04d3b0b3487a
eglelaume <egle.strockyte@gmail.com>
2018-01-14 00:12:22 -0800

    Complete exercise 02

---
+++ exercise-02/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+    public static void main(String[] args) {
+        System.out.println("+------------------------+");
+        System.out.println("|                        |");
+        System.out.println("|      CORNER STORE      |");
+        System.out.println("|                        |");
+        System.out.println("| 2018-01-13   00:03am   |");
+        System.out.println("|                        |");
+        System.out.println("| Gallons:        10.870 |");
+        System.out.println("| Price/gallons: $ 3.209 |");
+        System.out.println("|                        |");
+        System.out.println("| Fuel total:    $ 34.88 |");
+        System.out.println("|                        |");
+        System.out.println("+------------------------+");
+    }
+} 
