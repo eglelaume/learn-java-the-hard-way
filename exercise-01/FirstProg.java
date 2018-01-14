0ecd19f15405bf031fdb40892c19422e4544d004
Egle Strockyte <eglestrockyte@Egles-MBP.localdomain>
2018-01-13 23:51:55 -0800

    Complete first exercise

---
+++ exercise-01/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main(String[] args) {
+        System.out.println("Egle is determined to learn how to code.");
+        System.out.println("Today's date is January 13, 2018.");
+    }
+}
