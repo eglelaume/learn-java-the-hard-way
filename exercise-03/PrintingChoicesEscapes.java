972ba7c67a68a992b6c20b234aae29e194c7007e
eglelaume <egle.strockyte@gmail.com>
2018-01-14 23:03:27 -0800

    Complete exercise04 (extra task for exercise03)

---
+++ exercise-03/PrintingChoicesEscapes.java
@@ -0,0 +1,6 @@
+public class PrintingChoicesEscapes {
+    public static void main(String[] args) {
+        System.out.println("Alpha\nBravo\nCharlie\nDelta\n\nEchoFoxtrotGolf\nHotel\nIndia\n\n" +
+        "This is a test.\nI am learning Java the Hard Way!\n");
+    }
+}
