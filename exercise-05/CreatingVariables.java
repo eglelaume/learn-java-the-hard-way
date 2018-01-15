93468370c8bac5cc897699976d4c311f9bef1beb
eglelaume <egle.strockyte@gmail.com>
2018-01-14 23:31:30 -0800

    Complete exercise05

---
+++ exercise-05/CreatingVariables.java
@@ -0,0 +1,36 @@
+public class CreatingVariables {
+    public static void main(String[] args) {
+        int x, y, age;
+        double seconds, e, checking;
+        String firstName, lastName, title;
+        int hours;
+        double height;
+        String catName;
+
+        x = 10;
+        y = 400;
+        age = 39;
+        hours = 11;
+
+        seconds = 4.71;
+        e = 2.71828182845904523536;
+        checking = 1.89;
+        height = 1.83;
+
+        firstName = "Egle";
+        lastName = "Strockyte";
+        title = "Mrs.";
+        catName = "Yoko";
+
+
+        System.out.println("The variable x contains " + x);
+        System.out.println("The value " + y + " is stored in the variable y.");
+        System.out.println("The experiment took " + seconds + " seconds.");
+        System.out.println("A favorite irrational # is Euler's number: " + e);
+        System.out.println("Hopefully you have more than $" + checking + "!");
+        System.out.println("My name's " + title + " " + firstName + " " + lastName);
+        System.out.println("The time now is " + hours + "pm");
+        System.out.println("My height is " + height +"cm");
+        System.out.println("My cat's name is " + catName);
+    }
+}
