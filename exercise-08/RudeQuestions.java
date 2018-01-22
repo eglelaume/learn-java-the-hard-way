828d9979bf0dc8aa7b053596a28046953745c07b
eglelaume <egle.strockyte@gmail.com>
2018-01-21 16:24:58 -0800

    Update exercise-08

--- exercise-08/RudeQuestions.java
+++ exercise-08/RudeQuestions.java
@@ -2,25 +2,21 @@ import java.util.Scanner;
 
 public class RudeQuestions {
     public static void main( String[] args ) {
-        String name;
-        int age;
-        double weight, income;
-
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print( "Hello. What is your name? " );
-        name = keyboard.next();
+        String name = keyboard.next();
 
         System.out.print( "Hi, " + name + "! How old are you? " );
-        age = keyboard.nextInt();
+        int age = keyboard.nextInt();
 
         System.out.println( "So you're " + age + ", eh? That's not very old." );
         System.out.print( "How much do you weigh, " + name + "? " );
-        weight = keyboard.nextDouble();
+        double weight = keyboard.nextDouble();
 
         System.out.println( weight + "! Better keep that quiet!!" );
         System.out.print("Finally, what's your income, " + name + "? " );
-        income = keyboard.nextDouble();
+        double income = keyboard.nextDouble();
 
         System.out.print( "Hopefully that is " + income + " per hour" );
         System.out.println( " and not per year!" );
