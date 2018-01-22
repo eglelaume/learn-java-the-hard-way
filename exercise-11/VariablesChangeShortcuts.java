9b46878108ae8ae9675d60410aee6b73d666824e
eglelaume <egle.strockyte@gmail.com>
2018-01-21 17:39:26 -0800

    Complete exercise-11

---
+++ exercise-11/VariablesChangeShortcuts.java
@@ -0,0 +1,54 @@
+public class VariablesChangeShortcuts {
+    public static void main(String[] args) {
+        int i, j, k;
+
+        i = 5;
+        j = 5;
+        k = 5;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k);
+        i = i + 3;
+        j = j - 3;
+        k = k * 3;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k + "\n");
+
+        i = 5;
+        j = 5;
+        k = 5;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k);
+        i += 3;
+        j -= 3;
+        k *= 3;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k + "\n");
+
+        i = j = k = 5;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k);
+        i += 1;
+        j -= 2;
+        k *= 3;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k + "\n");
+
+        i = j = 5;
+        System.out.println("i: " + i + "\tj: " + j);
+        i =+ 1; //OOps!
+        j =- 2;
+        System.out.println("i: " + i + "\tj: " + j + "\n");
+
+        i = j = 5;
+        System.out.println("i: " + i + "\tj: " + j);
+        i++;
+        j--;
+        System.out.println("i: " + i + "\tj: " + j);
+
+        i = 5;
+        i -= 5;
+        System.out.println("i: " + i);
+
+        i = 5;
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
+        System.out.println("i: " + i);
+    }
+}
