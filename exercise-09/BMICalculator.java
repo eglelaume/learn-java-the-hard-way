7a3d2bd2dc3989a84d795c16b4155405a8c7681c
eglelaume <egle.strockyte@gmail.com>
2018-01-21 16:47:21 -0800

    WIP 2 exercise-09

--- exercise-09/BMICalculator.java
+++ exercise-09/BMICalculator.java
@@ -4,11 +4,14 @@ public class BMICalculator {
     public static void main( String[] args ) {
         Scanner keyboard = new Scanner(System.in);
 
-        System.out.print( "Your height in m: " );
-        double m = keyboard.nextDouble();
+        System.out.print( "Your height in inches: " );
+        double inches = keyboard.nextDouble();
 
-        System.out.print( "Your weight in kg: " );
-        double kg = keyboard.nextDouble();
+        System.out.print( "Your weight in pounds: " );
+        double pounds = keyboard.nextDouble();
+
+        double kg = pounds / 2.20462;
+        double m = inches / 39.3701;
 
         double bmi = kg / (m * m);
 
