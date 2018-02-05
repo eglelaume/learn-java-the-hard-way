73be2f8e855eb576b1cfbc0c9b9edf0778380e6d
eglelaume <egle.strockyte@gmail.com>
2018-02-04 21:39:12 -0800

    Complete exercise-21

---
+++ exercise-21/GenderTitles.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GenderTitles {
+    public static void main( String[] args ) {
+        Scanner keyboard = new Scanner(System.in);
+        String title;
+
+        System.out.print( "First name: " );
+        String first = keyboard.next();
+        System.out.print( "Last name: " );
+        String last = keyboard.next();
+        System.out.print( "Gender (M/F): " );
+        String gender = keyboard.next();
+        System.out.print( "Age: " );
+        int age = keyboard.nextInt();
+
+        if ( age < 20 ) {
+            title = first;
+        } else {
+            if ( gender.equals("F") ) {
+                System.out.print( "Are you married, "+first+"? (Y/N): " );
+                String married = keyboard.next();
+                if ( married.equals("Y") ) {
+                    title = "Mrs.";
+                } else {
+                    title = "Ms.";
+                }
+            } else {
+                title = "Mr.";
+            }
+        }
+
+        System.out.println( "\n" + title + " " + last );
+    }
+}
